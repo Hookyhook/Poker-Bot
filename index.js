172dd5c26556064aa43f855b93894bceef957552
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-01-26 18:56:50 +0100

    Big progress

--- index.js
+++ index.js
@@ -1,56 +1,58 @@
-const token = "MTA2NjcxNDUwNDk1MzU5Mzg4Ng.GNx_k8.zkmACJAUKSaEe67kpAENPmq2GwYH-yW9vpoVcU";
+const token =
+  "MTA2NjcxNDUwNDk1MzU5Mzg4Ng.Gn_tVb.Pc5rP5EQqUJCbAApKicHfu80QpIfaDM-d4y8K4";
 const applicationID = "1066714504953593886";
 
-const { REST, Routes, Embed, EmbedBuilder, channelLink, ReactionUserManager, InteractionCollector, ApplicationCommandOptionType, moveElementInArray } = require('discord.js');
+const {
+  REST,
+  Routes,
+  Embed,
+  EmbedBuilder,
+  channelLink,
+  ReactionUserManager,
+  InteractionCollector,
+  ApplicationCommandOptionType,
+  moveElementInArray,
+} = require("discord.js");
 
 const poker = require("./poker");
 
 const commands = [
   {
     name: "startgame",
-    description:"Starts a Game of Poker"
-  }
+    description: "Starts a Game of Poker",
+  },
 ];
 
 //
-const rest = new REST({ version: '10' }).setToken(token);
+const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
 
-    await rest.put(Routes.applicationCommands(applicationID), { body: commands });
+    await rest.put(Routes.applicationCommands(applicationID), {
+      body: commands,
+    });
 
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
 })();
-const { Client, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages,GatewayIntentBits.MessageContent] });
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
 
-
-
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isChatInputCommand()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isChatInputCommand()) return;
 
   if (interaction.commandName === "startgame") {
-    var table = poker.gameStart();
-    const embed = new EmbedBuilder()
-            .setColor('Grey')
-            .setTitle("New Game")
-            .addFields(
-                {
-                    name: "The cards on the table are:",
-                    value: table.card1+ " "+table.card2+" "+table.card3,
-                    inline: true,
-                }
-            );
-            interaction.reply({ embeds: [embed], ephemeral: true});
+    poker.gameStart(interaction);
   }
-
-
-
-}) 
+});
 client.login(token);
--- poker.js
+++ poker.js
@@ -1,36 +1,198 @@
-var deck = [];
-var table;
+const {
+  REST,
+  Routes,
+  Embed,
+  EmbedBuilder,
+  channelLink,
+  ReactionUserManager,
+  InteractionCollector,
+  ApplicationCommandOptionType,
+  moveElementInArray,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Events,
+  Collector,
+  Collection,
+} = require("discord.js");
 
-exports.gameStart = () =>{  
-    createDeck();
-    createTable();
-    return table;
-}
+var games = [];
 
+exports.gameStart = (interaction) => {
+  return newGame(interaction);
+};
 
-
-function createDeck(){
-    var colors = [":diamonds:", ":clubs:", ":spades:", ":hearts:"];
-    var numbers = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"];
-    for (const color of colors) {
-        for (const number of numbers) {
-            deck.push(color+" "+number);
+function newGame(interaction) {
+  if (searchGame(interaction) == undefined) {
+    var deck = createDeck();
+    var table = createTable(deck);
+    var gameid = interaction.user.id;
+    var members = [];
+    var member = {username: interaction.user.username ,memberid: interaction.user.id, hand: dealHand(deck) };
+    members.push(member);
+    var game = {
+      gameid: gameid,
+      members: members,
+      deck: deck,
+      table: table,
+    };
+    games.push(game);
+    const embed = new EmbedBuilder()
+      .setColor("Black")
+      .setTitle("New Game")
+      .addFields(
+          {
+              name: "WOW a new POKER GAME!",
+              value: "@here",
+              inline: false,
+        },
+        {
+          name: "Players Joined:",
+          value: interaction.user.username,
+          inline: false,
         }
+      );
+    let Buttons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("JOIN")
+        .setLabel("Join!")
+        .setStyle(ButtonStyle.Primary)
+    );
+    interaction.reply({
+      embeds: [embed],
+      ephemeral: false,
+      components: [Buttons],
+    });
+  } else {
+    const embed = new EmbedBuilder()
+      .setColor("Red")
+      .setTitle("Error")
+      .addFields({
+        name: "Channel Error",
+        value:
+          "You already created a game that is still active!!! Finish it before you start a new one!",
+        inline: true,
+      });
+    interaction.reply({ embeds: [embed], ephemeral: false });
     }
-    for (let index = 0; index < 1000; index++) {
-        deck.sort((a,b)=> 0.5-Math.random());
-    }
+    const collector = interaction.channel.createMessageComponentCollector();
+
+    collector.on("collect", async i => {
+        
+        if (i.user.id !== interaction.user.id) {
+            return;
+        }
+        if (i.message.interaction.id !== interaction.id) {
+            return
+        }
+        if (i.customId === "JOIN") {
+            addPlayer(i, interaction);
+            collector.stop();
+        }
+    });
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
 }
 
-function createTable(){
-    var card1 = deck.pop();
-    var card2 = deck.pop();
-    var card3 = deck.pop();
-    table = {card1, card2, card3};
+function createDeck() {
+  var d = [];
+  var colors = [":diamonds:", ":clubs:", ":spades:", ":hearts:"];
+  var numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A",
+  ];
+  for (const color of colors) {
+    for (const number of numbers) {
+      d.push(color + " " + number);
+    }
+  }
+  for (let index = 0; index < 1000; index++) {
+    d.sort((a, b) => 0.5 - Math.random());
+  }
+  return d;
 }
 
-function dealHand(){
-    var card1 = deck.pop();
-    var card2 = deck.pop();
-    return {card1,card2};
+function createTable(d) {
+  return {
+    card1: d.pop(),
+    card2: d.pop(),
+    card3: d.pop(),
+    card4: d.pop(),
+    card5: d.pop(),
+  };
+}
+
+function dealHand(d) {
+  return { card1: d.pop(), card2: d.pop() };
+}
+
+function addPlayer(i, interaction) {
+    var game = searchGame(interaction);
+    game.members.push({ username: i.user.username ,meberid: i.user.id, hand: dealHand(game.deck) });
+    var usernames = "";
+    for (const member of game.members) {
+        usernames += member.username + "\n";
+    }
+    const embed = new EmbedBuilder()
+      .setColor("Black")
+      .setTitle("New Game")
+      .addFields(
+          {
+              name: "WOW a new POKER GAME!",
+              value: "@here",
+              inline: false,
+        },
+        {
+          name: "Players Joined:",
+          value: usernames,
+          inline: false,
+        }
+      );
+    let Buttons = new ActionRowBuilder().addComponents(
+      new ButtonBuilder()
+        .setCustomId("JOIN")
+        .setLabel("Join!")
+        .setStyle(ButtonStyle.Primary)
+    );
+    interaction.editReply({
+      embeds: [embed],
+      ephemeral: false,
+      components: [Buttons],
+    })
+    const collector = interaction.channel.createMessageComponentCollector();
+    collector.on("collect", async i => {
+        
+        if (i.user.id !== interaction.user.id) {
+            return;
+        }
+        if (i.message.interaction.id !== interaction.id) {
+            return
+        }
+        if (i.customId === "JOIN") {
+            addPlayer(i, interaction);
+            collector.stop();
+        }
+    });
+    collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+}
+
+function searchGame(interaction) {
+  var searchCriteria = { gameid: interaction.user.id };
+
+  var foundGame = games.find((obj) => {
+    return Object.entries(searchCriteria).every(([key, value]) => {
+      return obj.hasOwnProperty(key) && obj[key] === value;
+    });
+  });
+  return foundGame;
 }
