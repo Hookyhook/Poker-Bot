20daebfb6ed24004eb43f01e15eff806243487e6
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-02-03 19:34:40 +0100

    Added documentation

--- poker.js
+++ poker.js
@@ -1,34 +1,26 @@
+//Importing all needed parts of the Discord API
 const {
-  REST,
-  Routes,
-  Embed,
   EmbedBuilder,
-  channelLink,
-  ReactionUserManager,
-  InteractionCollector,
-  ApplicationCommandOptionType,
-  moveElementInArray,
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
-  Events,
-  Collector,
-  Collection,
-  ThreadMemberFlagsBitField,
-  BaseSelectMenuBuilder,
-  InviteGuild,
 } = require("discord.js");
+//Importing the evaluation system
 const { determineHandRanking } = require("./evaluation");
+//Importing the pm2 module used to restart the bot as it is running on a server with pm2
 const pm2 = require("pm2");
-
+//Global variables
 let registeredUsers = [];
 var game = {};
-
+//The exported function that is called when the command is issued in Discord
 exports.gameStart = (interaction, client) => {
-  
+  //Creates a new game
   newGame(interaction);
+  //The collector get's all interactions that are used in the discord chat
   const collector = interaction.channel.createMessageComponentCollector();
+
   collector.on("collect", async (i) => {
+    //The switch starts the functions according to Id of the clicked button
     switch (i.customId) {
       case "JOIN":
         addPlayer(i, interaction);
@@ -37,9 +29,15 @@ exports.gameStart = (interaction, client) => {
         removePlayer(i, interaction, true);
         break;
       case "START":
+        /*
+         * The new round is only triggered if there are at least two players
+         * and the gameid is the id of the user who clicked the button
+         * because the gameid is the userid of the user who created the game
+         */
         if (i.user.id == game.gameid && game.members.length >= 2) {
           newRound(i, interaction, client);
         } else if (i.user.id != game.gameid) {
+          //If the user has not created the game an error message is send privat
           var embed = new EmbedBuilder()
             .setColor("Red")
             .setTitle("Error")
@@ -48,8 +46,10 @@ exports.gameStart = (interaction, client) => {
               value: "Only the creator of a game can start it",
               inline: true,
             });
+          //empheral: true let only the user that the message is sent to see the message
           i.reply({ embeds: [embed], ephemeral: true });
         } else {
+          //If there are not more than one player an error message is send privat
           var embed = new EmbedBuilder()
             .setColor("Red")
             .setTitle("Error")
@@ -61,8 +61,9 @@ exports.gameStart = (interaction, client) => {
           i.reply({ embeds: [embed], ephemeral: true });
         }
         break;
+      //The following interactions are for the ingame poker calls like FOLD, CHECK etc.
       case "FOLD":
-        setBet(i,  0, "Fold", client, interaction);
+        setBet(i, 0, "Fold", client, interaction);
         break;
       case "RAISE":
         for (const member of game.members) {
@@ -72,13 +73,12 @@ exports.gameStart = (interaction, client) => {
         }
         break;
       case "CHECK":
-        setBet(i,  game.highestbet, "Check", client, interaction);
+        setBet(i, game.highestbet, "Check", client, interaction);
 
         break;
       case "1":
         setBet(
           i,
-          
           parseInt(i.customId) + parseInt(game.highestbet),
           "Raised by " + i.customId + "$",
           client,
@@ -88,7 +88,7 @@ exports.gameStart = (interaction, client) => {
       case "5":
         setBet(
           i,
-          
+
           parseInt(i.customId) + parseInt(game.highestbet),
           "Raised by " + i.customId + "$",
           client,
@@ -99,7 +99,7 @@ exports.gameStart = (interaction, client) => {
       case "10":
         setBet(
           i,
-          
+
           parseInt(i.customId) + parseInt(game.highestbet),
           "Raised by " + i.customId + "$",
           client,
@@ -109,7 +109,7 @@ exports.gameStart = (interaction, client) => {
       case "50":
         setBet(
           i,
-          
+
           parseInt(i.customId) + parseInt(game.highestbet),
           "Raised by " + i.customId + "$",
           client,
@@ -119,34 +119,39 @@ exports.gameStart = (interaction, client) => {
       case "BACK":
         for (const member of game.members) {
           if (i.user.id == member.memberid) {
-            sendHand(i, member,  false);
+            sendHand(i, member, false);
           }
         }
         break;
-        case "ALLIN":
-          setBet(
-            i,
-            
-            0,
-            "ALLIN",
-            client,
-            interaction
-          );
-        break
+      case "ALLIN":
+        setBet(
+          i,
+
+          0,
+          "ALLIN",
+          client,
+          interaction
+        );
+        break;
     }
   });
-  collector.on("end", (collected) =>
-    console.log(`Collected ${collected.size} items`)
-  );
 };
-
+/*
+*This function is called the create a new game! 
+*/
 function newGame(interaction) {
+  //Checks whether the player already joined the game
   if (!checkforPlayer(interaction)) {
+    //Creates a whole card deck
     var deck = createDeck();
+    //Take out 5 cards
     var table = createTable(deck);
+    //Setting the gameid to the userdi
     var gameid = interaction.user.id;
     var members = [];
+    //Dealing the hand
     var hand = dealHand(deck);
+    //Creating the user with all properties
     var member = {
       username: interaction.user.username,
       memberid: interaction.user.id,
@@ -160,6 +165,7 @@ function newGame(interaction) {
       lastbalance: 100,
     };
     members.push(member);
+    //Creates the final game with all properties
     game = {
       gameid: gameid,
       members: members,
@@ -171,10 +177,13 @@ function newGame(interaction) {
       startinteraction: 0,
       firsthands: true,
     };
+    //Sends the first message
     i = undefined;
     updateStartMessage(i, interaction);
+    //Push the user to the registered user array that holds all players who play a poker game
     registeredUsers.push(interaction.user.id);
   } else {
+    //If there is already a game of the user an error is send
     var embed = new EmbedBuilder().setColor("Red").setTitle("Error").addFields({
       name: "Channel Error",
       value:
@@ -185,10 +194,12 @@ function newGame(interaction) {
     return;
   }
 }
-
+//Creates the poker deck
 function createDeck() {
   var d = [];
+  //All colors
   var colors = [":diamonds:", ":clubs:", ":spades:", ":hearts:"];
+  //All numbers
   var numbers = [
     "2",
     "3",
@@ -204,17 +215,19 @@ function createDeck() {
     "K",
     "A",
   ];
+  //Pushing to the deck all cards by combining every color with every number
   for (const color of colors) {
     for (const number of numbers) {
       d.push(color + " " + number);
     }
   }
+  //Mixing this deck 1000 times over a shuffle algorithm
   for (let index = 0; index < 1000; index++) {
-    d.sort((a, b) => 0.5 - Math.random());
+    d.sort(() => 0.5 - Math.random());
   }
   return d;
 }
-
+//Taking 5 cards out and return it in an object
 function createTable(d) {
   return {
     card1: d.pop(),
@@ -224,19 +237,21 @@ function createTable(d) {
     card5: d.pop(),
   };
 }
-
+//Taking 2 cards out and return it in an object
 function dealHand(d) {
   return { card1: d.pop(), card2: d.pop() };
 }
-
+//Add a Player
 function addPlayer(i, interaction) {
   var joined = false;
+  //Checks whether the player already joined
   for (const member of game.members) {
     if (i.user.id == member.memberid) {
       joined = true;
     }
   }
   var hand = dealHand(game.deck);
+  //Creating the user with all attributes
   if (!joined) {
     game.members.push({
       username: i.user.username,
@@ -250,8 +265,10 @@ function addPlayer(i, interaction) {
       balance: 100,
       lastbalance: 100,
     });
+    // Updating the start message with the new player
     updateStartMessage(i, interaction);
   } else {
+    //Sends an error if the user already joined
     var embed = new EmbedBuilder().setColor("Red").setTitle("Error").addFields({
       name: "Poker Game",
       value: "You already joined that game!",
@@ -266,6 +283,7 @@ function addPlayer(i, interaction) {
   }
 }
 
+//Checks whether a player is already registered in the registeredUser array
 function checkforPlayer(interaction) {
   var joined = false;
   for (const player of registeredUsers) {
@@ -275,16 +293,20 @@ function checkforPlayer(interaction) {
   }
   return joined;
 }
-
+//Removes a player from the gam
 function removePlayer(i, interaction, pregame) {
+  
   var joined = false;
-
   var embed;
+  //Checks whether the player is in the game
   for (const member of game.members) {
     if (member.id == i.user.id) {
       joined = true;
     }
   }
+  /**
+   * If the player is already in the game an error is send
+   */
   if (game.gameid == i.user.id && pregame) {
     embed = new EmbedBuilder().setColor("Red").setTitle("Error").addFields({
       name: "Poker Game",
@@ -292,8 +314,10 @@ function removePlayer(i, interaction, pregame) {
       inline: false,
     });
   } else if (!joined) {
+    //The array is filtered for every object that is not that player so he is removed
     searchValue = i.user.id;
     game.members = game.members.filter((obj) => obj.memberid !== searchValue);
+    //Than a confirmation is send
     embed = new EmbedBuilder()
       .setColor("Red")
       .setTitle("You left the game")
@@ -303,23 +327,27 @@ function removePlayer(i, interaction, pregame) {
         inline: false,
       });
   } else {
+    //If the player not even joined the game an error is send
     embed = new EmbedBuilder().setColor("Red").setTitle("Error").addFields({
       name: "Poker Game",
       value: "You can'not leave a game you did never join!",
       inline: false,
     });
   }
-  if(pregame){
-  updateStartMessage(i, interaction);
+  if (pregame) {
+    //If the game is still in the pregame the StartMessage is updated
+    updateStartMessage(i, interaction);
   }
+  //The embed is send
   i.followUp({
     embeds: [embed],
     ephemeral: true,
     components: [],
   });
 }
-
+//The start message is updated
 function updateStartMessage(i, interaction) {
+  //A string username is pushed with all game members and their last move
   var usernames = "";
   for (const member of game.members) {
     usernames += member.username + ": " + member.action + "\n";
@@ -338,7 +366,8 @@ function updateStartMessage(i, interaction) {
         value: usernames,
         inline: false,
       }
-    );
+  );
+  //This are the buttons of the startmessage
   let Buttons = new ActionRowBuilder().addComponents(
     new ButtonBuilder()
       .setCustomId("START")
@@ -353,7 +382,11 @@ function updateStartMessage(i, interaction) {
       .setLabel("Leave!")
       .setStyle(ButtonStyle.Primary)
   );
-
+/**
+ * If i is defined the message answers to it else the message answers to the interaction
+ * The difference is that i is an interaction triggered by a button the interaction
+ * ist the interaction of the command /startgame
+ */
   if (i != undefined) {
     i.replied = false;
     i.update({
@@ -370,15 +403,20 @@ function updateStartMessage(i, interaction) {
     });
   }
 }
+//This function starts a new round
 function newRound(i, interaction, client) {
+  //The round is counted up
   game.status++;
+  //If the round is 5 the game is finished and endGame is triggered
   if (game.status == 5) {
     endGame(i, interaction);
     return;
   }
+  //If game.status is 1 the current interaction is saved because it is user later
   if (game.status == 1) {
     game.startinteraction = i;
   }
+  //All values of the rounds before are resetted
   game.highestbet = 0;
   for (const member of game.members) {
     member.bet = 0;
@@ -387,6 +425,7 @@ function newRound(i, interaction, client) {
     member.lastbalance = member.balance;
   }
   game.members[0].turn = true;
+  //The cards string and username string are filled with the users and the cards on the table
   var usernames = "";
   var cards = "";
   for (const member of game.members) {
@@ -396,6 +435,7 @@ function newRound(i, interaction, client) {
       usernames += member.username + ": " + member.action + "\n";
     }
   }
+  //According to the round the number of card is shown the rest is filled up with white squares
   if (game.status == 1) {
     for (let index = 0; index < 5; index++) {
       cards += ":white_large_square:" + " ";
@@ -414,6 +454,7 @@ function newRound(i, interaction, client) {
       cards += ":white_large_square: :white_large_square:" + " ";
     }
   }
+  //The game message is prepared
   const embed = new EmbedBuilder()
     .setColor("Black")
     .setTitle("Game Started")
@@ -439,7 +480,7 @@ function newRound(i, interaction, client) {
         inline: false,
       }
     );
-
+//In the first round the origin start message is updated after this the game messages is edited
   if (game.status == 1) {
     game.startinteraction.update({
       embeds: [embed],
@@ -459,7 +500,9 @@ function newRound(i, interaction, client) {
   }
 }
 
+//This sends the hand
 function sendHand(i, member, raiseOptions) {
+  //The embed with the cards and the balance is prepared
   const embed = new EmbedBuilder()
     .setColor("White")
     .setTitle("Your hand")
@@ -519,12 +562,14 @@ function sendHand(i, member, raiseOptions) {
       .setLabel("50$")
       .setStyle(ButtonStyle.Primary)
   );
+  //If the user clicked on raise a new sets of buttons is prepared with the raise Options (1$ etc)
   var Buttons;
   if (raiseOptions) {
     Buttons = raiseButtons;
   } else {
     Buttons = normalButtons;
   }
+  //If it is the first time the hand is send it is send as a Follow Up after this it is updated
   if (game.firsthands) {
     member.interaction.followUp({
       embeds: [embed],
@@ -539,10 +584,11 @@ function sendHand(i, member, raiseOptions) {
     });
   }
 }
-
+//The bet is set
 function setBet(i, bet, action, client, interaction) {
   var turn = false;
   var allin = false;
+  //This checks whether the user how clicked the buttons is on the turn
   for (const member of game.members) {
     if (i.user.id == member.memberid && member.turn) {
       turn = true;
@@ -551,6 +597,10 @@ function setBet(i, bet, action, client, interaction) {
   if (turn) {
     for (const member of game.members) {
       if (i.user.id == member.memberid) {
+        /**
+         * If the bet is higher than the money before the round and
+         * the user is not ALL IN an error is send
+         */
         if (bet >= member.lastbalance && action != "ALLIN") {
           const embed = new EmbedBuilder()
             .setColor("Red")
@@ -560,37 +610,46 @@ function setBet(i, bet, action, client, interaction) {
               value: "You have not enough money",
               inline: false,
             });
-            i.reply({
-              embeds: [embed],
-              ephemeral: true
-            })
-            return;
+          i.reply({
+            embeds: [embed],
+            ephemeral: true,
+          });
+          return;
         }
-        if(action == "Fold"){
+        /*If the player folds he is removed from the game and the
+        *function returns
+        */
+        if (action == "Fold") {
           removePlayer(i, interaction, false);
-          if(game.members.length == 1){
+          if (game.members.length == 1) {
             endGame(i, interaction);
           }
           return;
         }
+        //The turn attribut of the next member is set to true
         const index = game.members.indexOf(member);
         game.members[index].turn = false;
         const nextIndex = (index + 1) % game.members.length;
         game.members[nextIndex].turn = true;
+        
         member.action = action;
-        if(action == "ALLIN"){
+        //If somebody goes allin the bet is set to the ballance
+        if (action == "ALLIN") {
           bet = member.lastbalance;
           allin = true;
         }
+        //The bet is removed from the balance and the pot is updated
         member.balance = member.lastbalance - bet;
         member.bet = bet;
         game.pot = parseInt(bet) + parseInt(game.pot);
-
+        //The hand with then updated balance is send
         sendHand(i, member, false);
       }
+      //If the bet is a raise the new Highest Bet is send
       if (bet > game.highestbet) {
         game.highestbet = bet;
       }
+      //The game message is updated
       var usernames = "";
       var cards = "";
       for (const member of game.members) {
@@ -653,9 +712,11 @@ function setBet(i, bet, action, client, interaction) {
         embeds: [embed],
         components: [],
       });
+      //The compare bet method is triggered
       compareBet(i, interaction, client, allin);
     }
   } else if (!turn) {
+    //If it is not the user's turn an error is send
     const embed = new EmbedBuilder()
       .setColor("Red")
       .setTitle("Error")
@@ -671,32 +732,35 @@ function setBet(i, bet, action, client, interaction) {
     });
   }
 }
-
+//If all players have the same bet so agree the next round is triggered
 function compareBet(i, interaction, client, allin) {
   var complete = true;
-  
+
   for (const member of game.members) {
     if (member.bet != game.highestbet || member.action == "waiting") {
       complete = false;
     }
   }
   if (complete) {
-    if(allin){
+    if (allin) {
       game.status = 4;
     }
     newRound(i, interaction, client);
   }
 }
-
+//This function ends the game
 async function endGame(i, interaction) {
+  //The players who have the highest evaluation are determined
   var highestevaluation = game.members.reduce((prev, current) => {
     if (prev.length === 0) return [current];
     if (prev[0].evaluation > current.evaluation) return prev;
     if (prev[0].evaluation < current.evaluation) return [current];
     return [...prev, current];
   }, []);
+  //The winners are set to a String together
   var winners = highestevaluation.map((member) => member.username).join(" ");
-  var win = game.pot/highestevaluation.length;
+  var win = game.pot / highestevaluation.length;
+  //The end message is send
   const embed = new EmbedBuilder()
     .setColor("Green")
     .setTitle("Winner")
@@ -737,14 +801,15 @@ async function endGame(i, interaction) {
     embeds: [embed],
     components: [],
   });
-   // restart the bot process
-   pm2.restart("2", (err, apps) => {
+  //The bot is completely restarted via the pm2 api to reset all vars
+  pm2.restart("pokerbot", (err) => {
     if (err) throw err;
 
     console.log("Bot restarted successfully");
   });
 }
 
+//The evaluation is prepared to be used in game and to be compared
 function evaluateHand(hand, table) {
   let result = determineHandRanking(hand, table);
   result = result.map((number) => {
