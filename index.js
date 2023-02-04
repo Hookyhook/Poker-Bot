b60aeec23baa177b9ecd7d91cc858d71aa801b8b
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-02-04 14:10:19 +0100

    final commits

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
+//This is the discord token for the bot it is not public so it is empty
 const token =
-  "MTA2NjcxNDUwNDk1MzU5Mzg4Ng.GPt_c6.lamsQ9uICyc37EMl2fkq6hn1kvuEqwHE3sIIGk";
+  "";
 const applicationID = "1066714504953593886";
 
+//Importing needed parts of the discord api
 const {
   REST,
   Routes,
-  Embed,
-  EmbedBuilder,
-  channelLink,
-  ReactionUserManager,
-  InteractionCollector,
-  ApplicationCommandOptionType,
-  moveElementInArray,
 } = require("discord.js");
 
+//Importing the poker game
 const poker = require("./poker");
 
+//Defining the game commands
 const commands = [
   {
     name: "startgame",
@@ -23,7 +20,7 @@ const commands = [
   },
 ];
 
-//
+//Needed actions to start up and login as an registered Discord bot
 const rest = new REST({ version: "10" }).setToken(token);
 
 (async () => {
